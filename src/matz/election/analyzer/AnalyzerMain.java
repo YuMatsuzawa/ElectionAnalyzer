fbbd144fb707874a54a607211f523be7d70ca4ff
YuMatsuzawa <celestial.rhapsody.0824@gmail.com>
2014-07-07 20:42:15 +0900

    package-info.java added. Basic counting tool initiated. Testbed implemented.

--- src/matz/election/analyzer/AnalyzerMain.java
+++ src/matz/election/analyzer/AnalyzerMain.java
@@ -1,28 +1,88 @@
-/**2013年7月に集めた選挙データを解析するためのパッケージ。<br>
- * 選挙時のツイートにおける集団性質の解析が主な目的だが、<br>
- * それ以前の収集したデータそのものの統計的な解析も含めるものとする。<br>
- * 
- */
 package matz.election.analyzer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.xml.soap.Text;
+
+import matz.election.analyzer.TweetCount.Map;
+import matz.election.analyzer.TweetCount.Reduce;
+import matz.election.analyzer.TweetCount.UserMap;
+import matz.election.analyzer.TweetCount.UserReduce;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapred.*;
 
 /**本パッケージにおいて、解析のためのエントリポイントとなるクラス。<br>
  * HDFS上のデータをMapReduceプログラムで処理すること、及び、<br>
  * jarに固めてサーバ上で走らせることを念頭に置いて開発されたし。<br>
  * 基本的な思想としては、実際の解析内容ごとにクラスを複数実装しておき、<br>
- * 本クラス内で希望する解析のためのクラスをインスタンス化して実行する。<br>
+ * 本クラス内で希望する解析のためのクラスをインスタンス化(ジョブ指定)して実行する。<br>
  * main関数内をその都度書き換えてビルドしなおしてもいいが、<br>
  * オプションと引数によってスイッチできるようにしておくのがベター。<br>
  * @author Matsuzawa
  *
  */
 public class AnalyzerMain {
-
+	protected final static String DEFAULT_INPUT = "/user/data/twitter2013july";
+	protected final static String DEFAULT_OUTPUT = "/user/matsuzawa/electionAnalyzer";
+	protected final static String[] COUNT_JOB_ARRAY = {
+		"TweetCount",
+		"UserTweetCount",
+		"HogeHoge"
+		};
+	protected final static ArrayList<String> COUNT_JOB_LIST = (ArrayList<String>)Arrays.asList(COUNT_JOB_ARRAY);
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args) throws Exception {
+		JobConf job = null;
+		if (args[0].isEmpty()) {
+			System.err.println("Specify jobClass:");
+			for(String jobName : COUNT_JOB_LIST) {
+				System.err.println(jobName);
+			}
+			System.exit(1);
+		} else {
+			if (COUNT_JOB_LIST.contains(args[0])) {
+				job = new JobConf(TweetCount.class);
+				if (!args[1].isEmpty() && !args[2].isEmpty()) {
+					FileInputFormat.setInputPaths(job, args[1]);
+					FileOutputFormat.setOutputPath(job, new Path(args[2]));
+				} else {
+					FileInputFormat.setInputPaths(job, DEFAULT_INPUT);
+					FileOutputFormat.setOutputPath(job, new Path(DEFAULT_OUTPUT));
+				}
+			}
+		}
+		if (args[0] == "TweetCount") {
+			job.setJobName(args[0]);
+			
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(IntWritable.class);
+					
+			job.setInputFormat(TextInputFormat.class);
+			job.setOutputFormat(TextOutputFormat.class);
+			
+			job.setMapperClass(Map.class);
+			job.setCombinerClass(Reduce.class);
+			job.setReducerClass(Reduce.class);
+		}
+		else if (args[0] == "UserTweetCount") {
+			job.setJobName(args[0]);
+			
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(IntWritable.class);
+					
+			job.setInputFormat(TextInputFormat.class);
+			job.setOutputFormat(TextOutputFormat.class);
+			
+			job.setMapperClass(UserMap.class);
+			job.setCombinerClass(UserReduce.class);
+			job.setReducerClass(UserReduce.class);
+		}
+		
 	}
 
 }
---
+++ src/matz/election/analyzer/TweetCount.java
@@ -0,0 +1,94 @@
+package matz.election.analyzer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+
+/**選挙データ解析の端緒としてとりあえずMapReduceプログラミングを練習。<br>
+ * 集めたデータのツイート数を数え上げる。ただし1行1ツイートになっているはずなので単に数えるだけならすぐ終わる。<br>
+ * そこで色々とメソッドを実装して、ツイートstatusに含まれる色々なキーに対してカウントを行えるようにしてみる。
+ * @author Matsuzawa
+ *
+ */
+public class TweetCount {
+	
+	/**最も単純に総ツイート数を数えるためのMapper。1行読むごとに加算する。従って最終的なK-Vペアは一組だけになる。
+	 * @author Matsuzawa
+	 *
+	 */
+	public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+		private static final IntWritable one = new IntWritable(1);
+		private Text tweetNum = new Text("tweetNum");
+		
+		@Override
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			output.collect(tweetNum, one); //単に1行読むごとに+1する。
+		}
+	}
+	
+	/**最も単純に総ツイート数を数えるためのReducer。<br>
+	 * Reducerには同一のKeyに対応する全てのValueが収められたIteratorであるValuesが渡される。これを「集計」するのがReducerの作業。<br>
+	 * 例えば上のMapでは単に"tweetNum"というKeyにひたすら"1"というValueが当てはめられたペアが大量に生成される。<br>
+	 * これを集計するということは、要は全ての"1"を合計して"tweetNum"Keyに対するValueとして当て直すことになる。
+	 * @author Matsuzawa
+	 *
+	 */
+	public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+		@Override
+		public void reduce(Text key, Iterator<IntWritable> values,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			int sum = 0;
+			while (values.hasNext()) sum += values.next().get();
+			output.collect(key, new IntWritable(sum));
+		}
+	}
+	
+	/**ユーザごとにツイート数を数えるためのMapper。単にその行のツイートJSONを読み、ユーザidをkeyとして読み込み、valueを+1する。<br>
+	 * Note:Interface Mapperの引数k1,v1,k2,v2のうちk1,v1の型と内容はInputFormatの分割形式に依存する。<br>
+	 * 例えばTextInputFormatなら行単位(k1はLongWritableのポインタ、v1は現在行のText)であり、Mapperは1行ごとに呼ばれる。<br>
+	 * これを前提としてMapperの作業内容を構築する。<br>
+	 * 返り値に当たるのはk2,v2で、これの形式はMapper内で指定し、これをOutputCollectorに渡す。<br>
+	 * @author Matsuzawa
+	 *
+	 */
+	public static class UserMap extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text user = new Text();
+		
+		@Override
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			// TODO JSON parse
+			
+		}
+		
+	}
+	
+	public static class UserReduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+
+		@Override
+		public void reduce(Text key, Iterator<IntWritable> values,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			// TODO 自動生成されたメソッド・スタブ
+			
+		}
+		
+	}
+
+	
+	public String usage() {
+		String usage = TweetCount.class.getSimpleName();
+		usage += "\t" + "Counts various values of keys in tweet status JSON.";
+		return usage;
+	}
+}

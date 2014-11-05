2530bda4a58d0a923032bda2e0410d1c6e61d40f
YuMatsuzawa <celestial.rhapsody.0824@gmail.com>
2014-11-05 21:31:50 +0900

    GraphAnalysis added

---
+++ src/matz/election/analyzer/GraphAnalysis.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package matz.election.analyzer;
+
+/**ツイートに含まれるURLの群とその言及ユーザの群からなる2部グラフから、URLのクラスタリングを行うクラス。<br>
+ * その他のネットワーク関連解析も取り扱う。
+ * @author YuMatsuzawa
+ *
+ */
+public class GraphAnalysis {
+
+}
